d4b4feb6b85a97d5ede2d2d7d8e5fceff3d22c11
LeonXiao587 <leonxiao587@gmail.com>
2019-03-19 22:43:23 -0600

    lease sign

--- cmpt496/ContractForm.Designer.vb
+++ cmpt496/ContractForm.Designer.vb
@@ -26,20 +26,20 @@ Partial Class ContractForm
         Me.PictureBox2 = New System.Windows.Forms.PictureBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.unitlable = New System.Windows.Forms.Label()
-        Me.BID = New System.Windows.Forms.TextBox()
-        Me.staffid = New System.Windows.Forms.TextBox()
+        Me.phone = New System.Windows.Forms.TextBox()
+        Me.id = New System.Windows.Forms.TextBox()
         Me.last_name = New System.Windows.Forms.TextBox()
         Me.Label9 = New System.Windows.Forms.Label()
         Me.Label8 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
         Me.first_name = New System.Windows.Forms.TextBox()
         Me.Label6 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.email = New System.Windows.Forms.TextBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.parking = New System.Windows.Forms.ComboBox()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Button1 = New System.Windows.Forms.Button()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
@@ -76,25 +76,25 @@ Partial Class ContractForm
         Me.unitlable.TabIndex = 56
         Me.unitlable.Text = "Unit: "
         '
-        'BID
+        'phone
         '
-        Me.BID.ForeColor = System.Drawing.SystemColors.ControlDark
-        Me.BID.Location = New System.Drawing.Point(234, 352)
-        Me.BID.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
-        Me.BID.Name = "BID"
-        Me.BID.Size = New System.Drawing.Size(314, 35)
-        Me.BID.TabIndex = 66
-        Me.BID.Text = "Enter Phone Number"
+        Me.phone.ForeColor = System.Drawing.SystemColors.ControlDark
+        Me.phone.Location = New System.Drawing.Point(234, 352)
+        Me.phone.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
+        Me.phone.Name = "phone"
+        Me.phone.Size = New System.Drawing.Size(314, 35)
+        Me.phone.TabIndex = 66
+        Me.phone.Text = "Enter Phone Number"
         '
-        'staffid
+        'id
         '
-        Me.staffid.ForeColor = System.Drawing.SystemColors.ControlDark
-        Me.staffid.Location = New System.Drawing.Point(234, 270)
-        Me.staffid.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
-        Me.staffid.Name = "staffid"
-        Me.staffid.Size = New System.Drawing.Size(314, 35)
-        Me.staffid.TabIndex = 65
-        Me.staffid.Text = "Enter ID Number"
+        Me.id.ForeColor = System.Drawing.SystemColors.ControlDark
+        Me.id.Location = New System.Drawing.Point(234, 270)
+        Me.id.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
+        Me.id.Name = "id"
+        Me.id.Size = New System.Drawing.Size(314, 35)
+        Me.id.TabIndex = 65
+        Me.id.Text = "Enter ID Number"
         '
         'last_name
         '
@@ -156,15 +156,15 @@ Partial Class ContractForm
         Me.Label6.TabIndex = 59
         Me.Label6.Text = "First name" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         '
-        'TextBox1
+        'email
         '
-        Me.TextBox1.ForeColor = System.Drawing.SystemColors.ControlDark
-        Me.TextBox1.Location = New System.Drawing.Point(234, 442)
-        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(314, 35)
-        Me.TextBox1.TabIndex = 68
-        Me.TextBox1.Text = "Enter Email Address"
+        Me.email.ForeColor = System.Drawing.SystemColors.ControlDark
+        Me.email.Location = New System.Drawing.Point(234, 442)
+        Me.email.Margin = New System.Windows.Forms.Padding(2, 4, 2, 4)
+        Me.email.Name = "email"
+        Me.email.Size = New System.Drawing.Size(314, 35)
+        Me.email.TabIndex = 68
+        Me.email.Text = "Enter Email Address"
         '
         'Label2
         '
@@ -205,14 +205,14 @@ Partial Class ContractForm
         Me.Label4.TabIndex = 71
         Me.Label4.Text = "Parking"
         '
-        'ComboBox1
+        'parking
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Items.AddRange(New Object() {"No", "Covered", "Outside"})
-        Me.ComboBox1.Location = New System.Drawing.Point(234, 514)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(179, 32)
-        Me.ComboBox1.TabIndex = 72
+        Me.parking.FormattingEnabled = True
+        Me.parking.Items.AddRange(New Object() {"No", "Covered", "Outside"})
+        Me.parking.Location = New System.Drawing.Point(234, 514)
+        Me.parking.Name = "parking"
+        Me.parking.Size = New System.Drawing.Size(179, 32)
+        Me.parking.TabIndex = 72
         '
         'Label5
         '
@@ -239,23 +239,23 @@ Partial Class ContractForm
         Me.GroupBox1.Controls.Add(Me.Label6)
         Me.GroupBox1.Controls.Add(Me.Label5)
         Me.GroupBox1.Controls.Add(Me.first_name)
-        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.parking)
         Me.GroupBox1.Controls.Add(Me.Label7)
         Me.GroupBox1.Controls.Add(Me.Label4)
         Me.GroupBox1.Controls.Add(Me.Label8)
         Me.GroupBox1.Controls.Add(Me.TextBox2)
         Me.GroupBox1.Controls.Add(Me.Label9)
         Me.GroupBox1.Controls.Add(Me.Label3)
-        Me.GroupBox1.Controls.Add(Me.staffid)
-        Me.GroupBox1.Controls.Add(Me.TextBox1)
-        Me.GroupBox1.Controls.Add(Me.BID)
+        Me.GroupBox1.Controls.Add(Me.id)
+        Me.GroupBox1.Controls.Add(Me.email)
+        Me.GroupBox1.Controls.Add(Me.phone)
         Me.GroupBox1.Controls.Add(Me.Label2)
         Me.GroupBox1.Location = New System.Drawing.Point(260, 110)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(585, 669)
         Me.GroupBox1.TabIndex = 75
         Me.GroupBox1.TabStop = False
-        Me.GroupBox1.Text = "GroupBox1"
+        Me.GroupBox1.Text = "Tenant Information"
         '
         'ContractForm
         '
@@ -269,7 +269,7 @@ Partial Class ContractForm
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox2)
         Me.Name = "ContractForm"
-        Me.Text = "ContractForm"
+        Me.Text = "Lease Signup"
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
@@ -281,20 +281,20 @@ Partial Class ContractForm
     Friend WithEvents PictureBox2 As PictureBox
     Friend WithEvents Label1 As Label
     Friend WithEvents unitlable As Label
-    Friend WithEvents BID As TextBox
-    Friend WithEvents staffid As TextBox
+    Friend WithEvents phone As TextBox
+    Friend WithEvents id As TextBox
     Friend WithEvents last_name As TextBox
     Friend WithEvents Label9 As Label
     Friend WithEvents Label8 As Label
     Friend WithEvents Label7 As Label
     Friend WithEvents first_name As TextBox
     Friend WithEvents Label6 As Label
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents email As TextBox
     Friend WithEvents Label2 As Label
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents parking As ComboBox
     Friend WithEvents Label5 As Label
     Friend WithEvents Button1 As Button
     Friend WithEvents GroupBox1 As GroupBox
--- cmpt496/ContractForm.vb
+++ cmpt496/ContractForm.vb
@@ -1,5 +1,12 @@
 ﻿Public Class ContractForm
     Private Sub ContractForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        parking.SelectedIndex = 0
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If first_name.Text = "" Or last_name.Text = "" Or phone.Text = "" Or email.Text = "" Or id.Text = "" Then
+            MsgBox("Input all fields please!")
+        End If
 
     End Sub
 End Class
